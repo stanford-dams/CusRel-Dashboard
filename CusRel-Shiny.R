c4f1b6c3480833ec1473771553fc37b5c86e7d3d
brand1923 <66454548+brand1923@users.noreply.github.com>
2021-03-03 23:54:38 -0800

    Added various checkboxes + dropdowns; Tried different basemap
    ForAction, RespondVia, Route, IncidentCity, Reasons checkboxes/dropdowns; Also adjusted popup to account for multiple reason columns

--- CusRel-Shiny.R
+++ CusRel-Shiny.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyWidgets)
 library(tidyverse)
 library(htmltools)
 library(sp)
@@ -22,23 +23,46 @@ cus_rel_data <- cus_rel_data %>%
                        "<b>Destination:</b> ", Destination , "<br/>",
                        "<b>Route:</b> ", Route, " &nbsp;&nbsp ", "<b>Vehicle #:</b> ", VehNo, "<br/>", 
                        "<b>Resolve Time:</b> ", ResolveTime, " day(s)<br/>", 
-                       "<b>Reason(s):</b> ", Reasons
-  )) # Add a Label for Popup
+                       "<b>Reason(s):</b> ", Reason1, if_else(is.na(Reason2), "", str_c("; ", Reason2))
+                       )) # Add a Label for Popup
 
 # Define UI
 ui <- fluidPage(
   
   # Application title
-  titlePanel("AC Transit customer complaints"),
+  titlePanel("AC Transit Customer Complaints"),
   
   sidebarLayout(
     sidebarPanel(
-      checkboxGroupInput(inputId="priorities", label="Priority", selected=c("Normal", "High"), choiceNames=c("Normal", "High"), choiceValues=c("Normal", "High"))
+      checkboxGroupInput(inputId="priorities", label="Priority", 
+                         selected=c("Normal", "High"), 
+                         choiceNames=c("Normal", "High"), 
+                         choiceValues=c("Normal", "High")),
+      pickerInput(inputId="cities", label="Incident City",
+                  choices=sort(unique(cus_rel_data$IncidentCity)),
+                  options=list('actions-box'=TRUE, 'live-search'=TRUE, size=5),
+                  multiple=TRUE),
+      pickerInput(inputId="routes", label="Route", 
+                  choices=sort(unique(cus_rel_data$Route)), 
+                  options=list('actions-box'=TRUE, 'live-search'=TRUE, size=5), 
+                  multiple=TRUE),
+      pickerInput(inputId="reasons", label="Complaint Reason", 
+                  choices=sort(unique(cus_rel_data %>% select("Reason1", "Reason2") %>% t %>% c %>% unique)), 
+                  options=list('actions-box'=TRUE, 'live-search'=TRUE, size=5), 
+                  multiple=TRUE),
+      checkboxGroupInput(inputId="respondVia", label="Respond Via", 
+                         selected=c("App", "Email", "Letter", "Phone", "None"), 
+                         choiceNames=c("App", "Email", "Letter", "Phone", "None"), 
+                         choiceValues=c("App", "Email", "Letter", "Phone", "None")),
+      pickerInput(inputId="department", label="Department", 
+                  choices=sort(unique(cus_rel_data$ForAction)), 
+                  options=list('actions-box'=TRUE, 'live-search'=TRUE, size=5),
+                  multiple=TRUE)
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      leafletOutput("the_map", height="800px"),
+      leafletOutput("the_map", height="700px"),
       plotOutput("distPlot")
     )
   )
@@ -46,14 +70,15 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output){
+  # Leaflet color palette
+  contact_sources <- unique(cus_rel_data$ContactSource) # get unique contact sources
+  contact_source_palette <- colorFactor(palette="Set3", domain=contact_sources)
+  # Map Output
   output$the_map <- renderLeaflet({
-    # Leaflet color palette
-    contact_sources <- unique(cus_rel_data$ContactSource) # get unique contact sources
-    contact_source_palette <- colorFactor(palette="Set3", domain=contact_sources)
     color_list <- contact_source_palette(contact_sources)
     cus_rel_data %>%
       leaflet() %>%
-        addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
+        addProviderTiles(providers$CartoDB.Positron) %>%
         addCircleMarkers(lat = ~Latitude, lng = ~Longitude, 
                          fill = TRUE, fillColor = ~contact_source_palette(ContactSource), 
                          fillOpacity = 0.6, stroke = FALSE, 
@@ -62,13 +87,21 @@ server <- function(input, output){
                          group = "circlemarkers") %>%
         addLegend("bottomright", colors = color_list, labels = contact_sources, title = "Contact Source")
   })
+  # Filter Data Reactively without Shifting Map
   filtered_data <- reactive(
-    filter(cus_rel_data, Priority %in% input$priorities)
+    filter(cus_rel_data, 
+           Priority %in% input$priorities,
+           IncidentCity %in% input$cities, 
+           Route %in% input$routes,
+           RespondVia %in% input$respondVia, 
+           ForAction %in% input$department, 
+           Reason1 %in% input$reasons | Reason2 %in% input$reasons)
   )
+  # Update Map After Options are Changed
   observeEvent(filtered_data(), {
     proxy <- leafletProxy("the_map", data=filtered_data()) %>%
       clearGroup("circlemarkers") %>%
-      addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
+      addProviderTiles(providers$CartoDB.Positron) %>%
       addCircleMarkers(lat = ~Latitude, lng = ~Longitude, 
                        fill = TRUE, fillColor = ~contact_source_palette(ContactSource), 
                        fillOpacity = 0.6, stroke = FALSE, 
